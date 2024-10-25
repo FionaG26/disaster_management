ff4b086171fdf241ce38de14737b669a1ec72218
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-10-25 16:31:06 +0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Disaster Management System - Kenya</h1>
+        </header>
+    );
+}
+
+export default Header;
